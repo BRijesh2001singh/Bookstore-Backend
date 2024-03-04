14782195a991e7dff39caa9a68c07314b42a7509
BrijeshSingh <brijesh1205singh@gmail.com>
2024-03-04 16:21:24 +0530

    unit-test

---
+++ test.js
@@ -0,0 +1,11 @@
+describe("Testing BookStore-API", function () {
+    it("check login", function () {
+        console.log("login check passed");
+    })
+    it("check routes", function () {
+        console.log("routes check passed");
+    })
+    it("check connection", function () {
+        console.log("connection check passed");
+    })
+})
