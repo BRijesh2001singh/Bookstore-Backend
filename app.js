b4eb6cce969aa448e53904a2fc8e38ca0bf015ca
BrijeshSingh <brijesh1205singh@gmail.com>
2024-09-14 01:10:45 +0530

    features

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const bookroute = require("./routes/bookroutes");
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 require("./connection/conncetion");
-
+app.use(cookieParser());
 app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:5000', 'https://chimerical-puffpuff-55489b.netlify.app'],
   credentials: true,
